38f1d47fa41419ed2483402af5082291a5526095
Drakonkinst <drakonkinstmc@gmail.com>
2020-06-16 00:34:53 -0700

    Initial Unity prototype
    - Placeholder models
    - Basic player movement in world
    - Isometric camera movement for world navigation

---
+++ Assets/Scripts/PlayerWorld.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWorld : MonoBehaviour
+{
+    public float moveSpeed = 5F; // units/sec
+    public float rotationSpeed = 1000F;
+    public Transform anchor;
+    
+    private Vector3 moveDirection;
+    private Transform myTransform;
+
+    void Start() {
+        myTransform = transform;
+    }
+
+    void Update() {
+        GetInput();
+        MoveCharacter();
+    }
+
+    private void GetInput() {
+        moveDirection = Vector3.zero;
+        if(Input.GetKey(KeyCode.W)) moveDirection += anchor.forward;
+        if(Input.GetKey(KeyCode.S)) moveDirection += -anchor.forward;
+        if(Input.GetKey(KeyCode.A)) moveDirection += -anchor.right;
+        if(Input.GetKey(KeyCode.D)) moveDirection += anchor.right;
+        moveDirection.y = 0.0F;
+    }
+
+    private void MoveCharacter() {
+        // set rigidbody speed instead of changing position
+        GetComponent<Rigidbody>().velocity = moveDirection.normalized * moveSpeed;
+
+        if(moveDirection != Vector3.zero) {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        // use for tracking towns and such
+        if(col.gameObject.CompareTag("Wall")) {
+            //Debug.Log("Ouch");
+            
+        }
+        
+    }
+}
