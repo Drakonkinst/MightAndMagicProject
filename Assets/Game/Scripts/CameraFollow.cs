c8c246e3393dc25e8449f988b6effa41f7d72364
Drakonkinst <drakonkinstmc@gmail.com>
2020-06-16 23:15:48 -0700

    Renamed walkDistance to playerDistance
    - Removed runDistance

--- Assets/Game/Scripts/CameraFollow.cs
+++ Assets/Game/Scripts/CameraFollow.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-    public Transform target;
-    public float walkDistance;
-    public float runDistance;
-    public float height;
+    public Transform target;          // The target to follow
+    public float playerDistance;      // The distance from the target
+    public float height;              // The height of the camera above the target
 
     private Transform myTransform;
 
-    // Start is called before the first frame update
     void Start()
     {
         if(target == null) {
@@ -22,10 +20,9 @@ public class CameraFollow : MonoBehaviour {
         UpdateIsometric();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Nothing here
     }
 
     void LateUpdate() {
@@ -34,35 +31,8 @@ public class CameraFollow : MonoBehaviour {
     }
 
     private void UpdateIsometric() {
-        myTransform.position = target.position + new Vector3(0, height, -walkDistance);
+        myTransform.position = target.position + new Vector3(0, height, -playerDistance);
         myTransform.LookAt(target.position);
 
     }
-
-    /*
-        playerMoveDir = player.transform.position - playerPrevPos;
-    if (playerMoveDir != Vector3.zero)
-    {
-        playerMoveDir.normalize();
-        transform.position = player.transform.position - playerMoveDir * distance;
-
-        transform.position.y += 5f; // required height
-
-        transform.LookAt(player.transform.position);
-
-        playerPrevPos = player.transform.position;
-    }*/
-    /*
-        Vector3 playerMoveDir = target.position - playerPrevPos;
-        if(playerMoveDir != Vector3.zero) {
-            playerMoveDir.Normalize();
-            
-            //myTransform.position = new Vector3(target.position.x, target.position.y + height, target.position.z - walkDistance);
-            myTransform.position = target.position - playerMoveDir * walkDistance;
-            myTransform.position += new Vector3(0, height, 0);
-            myTransform.LookAt(target.position);
-            playerPrevPos = target.position;
-        }
-        
-        */
 }
