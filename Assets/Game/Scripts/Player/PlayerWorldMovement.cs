cafcc881809d715878859a0bda2058408ca3c50a
Drakonkinst <drakonkinstmc@gmail.com>
2020-06-17 15:45:10 -0700

    Renamed files

--- Assets/Game/Scripts/Player/PlayerWorldMovement.cs
+++ Assets/Game/Scripts/Player/PlayerWorldMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerWorld : MonoBehaviour
+public class PlayerWorldMovement : MonoBehaviour
 {
     public float moveSpeed = 5F;
     public float rotationSpeed = 1000F;
