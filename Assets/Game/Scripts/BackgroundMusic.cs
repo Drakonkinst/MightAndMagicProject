4d0031aafa523731a2e5de2dc9b02fead6e11c4f
Drakonkinst <drakonkinstmc@gmail.com>
2020-06-17 21:32:47 -0700

    Added rotating background music
    - Uses a custom Sound Manager object, perhaps inefficiently since we really want to keep the object
    - Created a Sound Manager object that allows multiple sounds to be played from the same source using self-destructing child objects

---
+++ Assets/Game/Scripts/BackgroundMusic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public AudioClip[] music;
+    public float volume = 0.55f;
+    
+    private AudioSource currentMusic = null;
+    private Transform myTransform;
+    private int currentIndex = -1;
+
+    void Start() {
+        if(music == null || music.Length == 0) {
+            Debug.LogWarning("No background music found!");
+            return;
+        }
+        
+        myTransform = transform;
+        PlayNextSong();
+        currentMusic.loop = true;
+    }
+    
+    private void PlayNextSong() {
+        AudioClip clip = ChooseRandomSong();
+        Debug.Log("Playing " + clip.name);
+        // Creates new object. Probably inefficient but I'm too tired to think of a natural system for now.
+        // Maybe just use the native system for the background music? Keep an AudioSource object
+        currentMusic = SoundManager.Instance.Play(clip, myTransform, volume);
+        Invoke("PlayNextSong", currentMusic.clip.length);
+    }
+    
+    private AudioClip ChooseRandomSong() {
+        int newIndex;
+        do {
+            newIndex = Random.Range(0, music.Length);
+        } while(newIndex == currentIndex);
+        return music[newIndex];
+    }
+}
